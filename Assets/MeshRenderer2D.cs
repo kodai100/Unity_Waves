53faf32656be4f9af099736eff463232a7a551b3
KodaiTakao <takaokodai@team-lab.com>
2017-04-18 15:35:36 +0900

    Initial Commit

---
+++ Assets/MeshRenderer2D.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace kodai.Wave.Two {
+
+    public class MeshRenderer2D : MonoBehaviour {
+
+        public WaveSimulation script;
+        Mesh mesh;
+        
+        void Start() {
+            GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        }
+
+        void Update() {
+            CreateVertices();
+        }
+
+        void CreateVertices() {
+            
+            mesh.name = "Procedural Grid";
+
+            Vector3[] vertices = new Vector3[(script.num+1) * (script.num+1)];
+
+            int i = 0;
+            for (int y = 0; y <= script.num; y++) {
+                for (int x = 0; x <= script.num; x++) {
+                    vertices[i] = new Vector3(x, script.u_new[y+1,x+1] * 10, y);
+                    i++;
+                }
+            }
+            mesh.vertices = vertices;
+
+            int[] triangles = new int[script.num * script.num * 6];
+            for (int ti = 0, vi = 0, y = 0; y < script.num; y++, vi++) {
+                for (int x = 0; x < script.num; x++, ti += 6, vi++) {
+                    triangles[ti] = vi;
+                    triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                    triangles[ti + 4] = triangles[ti + 1] = vi + script.num+ 1;
+                    triangles[ti + 5] = vi + script.num + 2;
+                }
+            }
+            mesh.triangles = triangles;
+
+            mesh.RecalculateNormals();
+            mesh.RecalculateBounds();
+        }
+    }
+}
---
+++ Assets/WaveSimulation.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace kodai.Wave.Two {
+
+    public class WaveSimulation : MonoBehaviour {
+
+        public int num = 256;
+
+        public float dt = 1/60f;
+        public float dd = 0.1f;
+        public float c = 1;
+
+        private float[,] u_old;
+        private float[,] u_tmp;
+        public float[,] u_new;
+
+        Texture2D texture;
+        
+        void Start() {
+            u_old = new float[num + 2, num+2]; // 境界分を余分にとる
+            u_tmp = new float[num + 2, num + 2];
+            u_new = new float[num + 2, num + 2];
+
+            texture = new Texture2D(256,256);
+            texture.filterMode = FilterMode.Point;
+
+            Initialize();
+
+            ApplyTexture();
+        }
+        
+        void Update() {
+
+            for (int y = 1; y < num + 1; y++) {
+                for (int x = 1; x < num + 1; x++) {
+                    u_new[y, x] = 2f * u_tmp[y, x] - u_old[y, x] + c * c * dt * dt / (dd * dd) * (u_tmp[y + 1, x] + u_tmp[y - 1, x] + u_tmp[y, x + 1] + u_tmp[y, x - 1] - 4.0f * u_tmp[y,x]);
+                }
+            }
+
+            if (Input.GetMouseButtonUp(0)) {
+                for (int y = 1; y < num + 1; y++) {
+                    for (int x = 1; x < num + 1; x++) {
+                        if (Vector2.Distance(new Vector2(num / 2, num / 2), new Vector2(x, y)) < 10) {
+                            u_new[y, x] = 0.1f;
+                        }
+                    }
+                }
+                
+            }
+
+            SetBoundaryCondition();
+
+            ApplyTexture();
+            
+            FlipBuffer();
+        }
+
+        void SetBoundaryCondition() {
+            for(int x = 0; x < num + 2; x++) {
+                u_old[0, x] = 0;
+                u_old[num + 1, x] = 0;
+                u_tmp[0, x] = 0;
+                u_tmp[num + 1, x] = 0;
+                u_new[0, x] = 0;
+                u_new[num + 1, x] = 0;
+            }
+
+            for (int y = 0; y < num + 2; y++) {
+                u_old[y, 0] = 0;
+                u_old[y, num + 1] = 0;
+                u_tmp[y, 0] = 0;
+                u_tmp[y, num + 1] = 0;
+                u_new[y, 0] = 0;
+                u_new[y, num + 1] = 0;
+            }
+        }
+
+        void Initialize() {
+
+            // Initialize u0
+            for(int y = 1; y<num + 1; y++) {
+                for (int x = 1; x < num + 1; x++) {
+                    u_old[y, x] = 0;
+                    u_tmp[y, x] = 0;
+                    u_new[y, x] = 0;
+                    if(Vector2.Distance(new Vector2(num/2, num/2), new Vector2(x, y)) < 10) {
+                        u_old[y, x] = 1;
+                    }
+                }
+            }
+
+
+            for (int y = 1; y < num + 1; y++) {
+                for (int x = 1; x < num + 1; x++) {
+                    u_tmp[y, x] = u_old[y, x] + c * c / 2.0f * dt * dt / (dd * dd) * (u_old[y + 1, x] + u_old[y - 1, x] + u_old[y, x + 1] + u_old[y, x - 1] - 4.0f * u_old[y,x]);
+                }
+            }
+            SetBoundaryCondition();
+
+        }
+
+        void FlipBuffer() {
+            for (int x = 0; x < num + 2; x++) {
+                for (int y = 0; y < num + 2; y++) {
+                    u_old[y, x] = u_tmp[y, x];
+                    u_tmp[y, x] = u_new[y, x];
+                }
+            }
+        }
+
+        void ApplyTexture() {
+            for (int y = 1; y < num + 1; y++) {
+                for (int x = 1; x < num + 1; x++) {
+                    texture.SetPixel(x - 1, y - 1, new Color(u_new[y,x]/2f +0.5f, u_new[y, x] / 2f + 0.5f, u_new[y, x] / 2f + 0.5f));
+                }
+            }
+            texture.Apply();
+        }
+    }
+}
